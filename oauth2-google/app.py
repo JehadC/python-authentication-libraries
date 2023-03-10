76ad40f3113102b18e118c67fec16aa8dcd22083
Jehad Cemanes <jehad.cemanes@gmail.com>
2023-03-11 06:06:47 +0800

    modify app.py

--- oauth2-google/app.py
+++ oauth2-google/app.py
@@ -7,32 +7,44 @@ from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
 
+# Disable insecure transport warning
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+# Create a Flask app instance
 app = Flask(__name__)
 app.secret_key = 'SECRET-KEY'
 
-client_secrets_file = os.path.join(
+# Define location of client secrets file
+CLIENT_SECRETS_FILE = os.path.join(
     pathlib.Path(__file__).parent,
     'client_secret_file.json'
 )
 
+# Define the scopes for the flow object
+SCOPES = [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'openid'
+]
+
+# Define the redirect URI for the flow object
+REDIRECT_URI = 'http://127.0.0.1:5000/callback'
+
+# Initialize the Google OAuth2 flow
 google_oauth2_flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=[
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email',
-        'openid'
-    ],
-    redirect_uri='http://127.0.0.1:5000/callback'
+    client_secrets_file = CLIENT_SECRETS_FILE,
+    scopes = SCOPES,
+    redirect_uri = REDIRECT_URI
 )
 
 
+# Define a root route.
 @app.route('/')
 def index():
     return redirect(url_for('login'))
 
 
+# Define a route for login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -40,6 +52,7 @@ def login():
     return render_template('login.html')
 
 
+# Define a route for authorization
 @app.route('/authorization')
 def authorization():
     authorization_url, state = google_oauth2_flow.authorization_url(
@@ -48,13 +61,13 @@ def authorization():
     return redirect(authorization_url)
 
 
+# Define a route for callback
 @app.route("/callback")
 def callback():
     google_oauth2_flow.fetch_token(authorization_response=request.url)
     if not session['state'] == request.args['state']:
         abort(500)
     credentials = google_oauth2_flow.credentials
-    print(str(credentials))
     request_session = requests.session()
     cached_session = cachecontrol.CacheControl(request_session)
     token_request = google.auth.transport.requests.Request(
@@ -69,16 +82,19 @@ def callback():
     return redirect('/dashboard')
 
 
+# Define a route for dashboard
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 
+# Define a route for logout
 @app.route('/logout')
 def logout():
     session.clear()
-    return render_template(url_for('login'))
+    return redirect(url_for('login'))
 
 
+# Start the Flask app if the script is run directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
